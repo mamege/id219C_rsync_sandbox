24d6a5579e935db2a1ed97b17f90670c3d5306b1
Ryuta KASHINO <kashino@work.hpcs.cs.tsukuba.ac.jp>
2021-09-25 16:51:21 +0900

    現在の状況に合わせた

--- exec.sh
+++ exec.sh
@@ -1,24 +1,23 @@
 #--- 毎回 変数設定 start ---#
-WORK_DIR=/tmp/workdir
-TMP_INTERNAL=${WORK_DIR:?"undefined"}/src
-TMP_PUBLIC=${WORK_DIR:?"undefined"}/dest
-# set | (grep TMP_ || echo "Error: $TMP_* is Undefined") # TMP_を含む一時変数が定義されていることを確認
+INTERNAL=/home/www/new/https/internal/bachelor
+PUBLIC=/home/www/new/https/bachelor
 
-TARGET=test.txt
+WORK_DIR=/tmp/workdir
+TMP_INTERNAL=${WORK_DIR:?"undefined"}/internal_bachelor
+TMP_PUBLIC=${WORK_DIR:?"undefined"}/public_bachelor
+BACKUP_DIR=${TMP_PUBLIC}/$(date +%Y)
+
+TARGET=arcteam.md
+# set | (grep TMP_ || echo "Error: $TMP_* is Undefined") # TMP_を含む一時変数が定義されていることを確認
 #--- 毎回 変数設定 end ---#
 
 function init() {
     echo "-- initialize --"
     #--- 1回目のみ 初期設定 start ---#
     mkdir -p ${TMP_INTERNAL:?"undefined"}/ ${TMP_PUBLIC:?"undefined"}/
-    echo "origin" > $TMP_PUBLIC/$TARGET
-    chgrp HPCS-WEB $TMP_PUBLIC/*
+    cp -r ${PUBLIC:?"undefined"}/* $TMP_PUBLIC/ 2>/dev/null
+    cp -r ${INTERNAL:?"undefined"}/* $TMP_INTERNAL/
 
-    echo "1st edit" > $TMP_INTERNAL/$TARGET
-    echo "same file" > $TMP_INTERNAL/same_contents_file.txt
-
-    # 余計なdirctory
-    mkdir -p $TMP_INTERNAL/deleteme && echo "deleteme" > $TMP_INTERNAL/deleteme/deleteme.txt
 }
 
 function del() {
@@ -59,22 +58,26 @@ del && init
 # -v: --verbose 詳細情報を表示
 # -h: --human-readable
 
-BACKUP_DIR=${TMP_PUBLIC}/$(date +%Y)
+echo "origin" >> $TMP_PUBLIC/$TARGET
 backup $TMP_PUBLIC "$BACKUP_DIR";sleep 1
 
-rsync -rlth \
+echo "1st@internal" >> $TMP_INTERNAL/$TARGET
+rsync -rltvh \
   --exclude='.*' --exclude='*/' --include='*.*'\
  ${TMP_INTERNAL:?"undefined"}/ ${TMP_PUBLIC:?"undefined"}/
 
 backup $TMP_PUBLIC "$BACKUP_DIR";sleep 1
-
-echo "2nd edit" > $TMP_INTERNAL/$TARGET
-rsync -rlth \
+echo "2st@internal" >> $TMP_INTERNAL/$TARGET
+rsync -rltvh \
   --exclude='.*' --exclude='*/' --include='*.*'\
  ${TMP_INTERNAL:?"undefined"}/ ${TMP_PUBLIC:?"undefined"}/
 
 backup $TMP_PUBLIC "$BACKUP_DIR";sleep 1
+echo "3rd@internal" >> $TMP_INTERNAL/$TARGET
+rsync -rltvh \
+  --exclude='.*' --exclude='*/' --include='*.*'\
+ ${TMP_INTERNAL:?"undefined"}/ ${TMP_PUBLIC:?"undefined"}/
 
-echo $TMP_PUBLIC
-ls -l --full-time $TMP_PUBLIC/
-find $BACKUP_DIR/ -type f | grep $TARGET | xargs -L 1 -t cat
+echo $BACKUP_DIR 
+ls -l --full-time $BACKUP_DIR/ | grep -v -E "md|jpg|png|txt|html|pdf"
+find $TMP_PUBLIC/ -type f | grep $TARGET | xargs -L 1 -t tail -n 2
