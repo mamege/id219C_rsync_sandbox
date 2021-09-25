9c979f447c1b49e7ff585eed661377681a2e7af3
Ryuta KASHINO <kashino@work.hpcs.cs.tsukuba.ac.jp>
2021-09-25 10:05:40 +0900

    rsyncに与えるsrc, dstにおける、/の有無が与える影響を調査
    以下の通り、/を付与した場合は、srcの中身がcopyされる。
    付与しない場合は、src自体がcopyされる。
    $ ./exec.sh
    TMP_INTERNAL=/tmp/workdir/src
    TMP_PUBLIC=/tmp/workdir/dest
    -- initialize --
    -rw-r--r-- 1 kashino HPCS-WEB 7 2021-09-25 10:05:13.202916812 +0900 /tmp/workdir/dest/test.txt
    rsync <option> src/ dst/
    合計 8
    -rw-r--r-- 1 kashino HPCS 10 2021-09-25 10:05:14.221936181 +0900 same_contents_file.txt
    -rw-r--r-- 1 kashino HPCS  9 2021-09-25 10:05:14.221936181 +0900 test.txt
    1st edit
    -- initialize --
    -rw-r--r-- 1 kashino HPCS-WEB 7 2021-09-25 10:05:14.253936789 +0900 /tmp/workdir/dest/test.txt
    rsync <option> src dst
    合計 4
    drwxr-xr-x 2 kashino HPCS     50 2021-09-25 10:05:15.270956121 +0900 src
    -rw-r--r-- 1 kashino HPCS-WEB  7 2021-09-25 10:05:14.253936789 +0900 test.txt
    origin

---
+++ exec.sh
@@ -0,0 +1,48 @@
+#--- 毎回 変数設定 start ---#
+WORK_DIR=/tmp/workdir
+TMP_INTERNAL=${WORK_DIR:?"undefined"}/src
+TMP_PUBLIC=${WORK_DIR:?"undefined"}/dest
+set | (grep TMP_ || echo "Error: $TMP_* is Undefined") # TMP_を含む一時変数が定義されていることを確認
+
+TARGET=test.txt
+#--- 毎回 変数設定 end ---#
+
+function init() {
+    echo "-- initialize --"
+    #--- 1回目のみ 初期設定 start ---#
+    mkdir -p ${TMP_INTERNAL:?"undefined"}/ ${TMP_PUBLIC:?"undefined"}/
+    echo "origin" > $TMP_PUBLIC/$TARGET
+    chgrp HPCS-WEB $TMP_PUBLIC/* && ls $_ -l --full-time
+    sleep 1
+    echo "1st edit" > $TMP_INTERNAL/$TARGET
+    echo "same file" > $TMP_INTERNAL/same_contents_file.txt
+}
+
+function del() {
+    rm -rf ${TMP_INTERNAL:?"undefined"} ${TMP_PUBLIC:?"undefined"} 
+}
+
+del && init 
+
+# -r: --links 指定パスのみならず、配下にあるファイルおよびディレクトリをcopy
+# -l: --recursive シンボリックリンクをシンボリックリンクのままコピー
+# -v: --verbose 詳細情報を表示
+# -h: ?? 読みやすくする
+echo "rsync <option> src/ dst/"
+rsync -rlth \
+ ${TMP_INTERNAL:?"undefined"}/ ${TMP_PUBLIC:?"undefined"}/
+ls -l --full-time $TMP_PUBLIC/ && cat $TMP_PUBLIC/$TARGET
+
+
+del && init  
+
+# -r: --links 指定パスのみならず、配下にあるファイルおよびディレクトリをcopy
+# -l: --recursive シンボリックリンクをシンボリックリンクのままコピー
+# -v: --verbose 詳細情報を表示
+# -h: ?? 読みやすくする
+echo "rsync <option> src dst"
+rsync -rlth \
+ ${TMP_INTERNAL:?"undefined"} ${TMP_PUBLIC:?"undefined"}
+ls -l --full-time $TMP_PUBLIC/ && cat $TMP_PUBLIC/$TARGET
+
+del 
